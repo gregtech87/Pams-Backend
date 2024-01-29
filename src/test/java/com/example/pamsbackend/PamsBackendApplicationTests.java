367414926e611df84c0e0747c1fb4f254e4943eb
gregtech87 <129724554+gregtech87@users.noreply.github.com>
2024-01-29 10:25:50 +0100

    initial commit

---
+++ src/test/java/com/example/pamsbackend/PamsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pamsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PamsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
